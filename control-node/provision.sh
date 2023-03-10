069b11d0086d220cfe4432fad2cb44deff000ca8
Yago Alexandre <goltarayago@gmail.com>
2023-03-10 13:02:50 -0300

    Adding hosts to /etc/hosts via provision.sh

--- control-node/provision.sh
+++ control-node/provision.sh
@@ -3,3 +3,8 @@ echo "Installing EPEL"
 sudo yum install -y epel-release >/dev/null 2>&1
 echo "Installing Ansible"
 sudo yum install -y ansible >/dev/null 2>&1
+cat <<EOT >> /etc/hosts
+192.168.56.2 control-node
+192.168.56.3 app01
+192.168.56.4 db01
+EOT
