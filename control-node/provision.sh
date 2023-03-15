4eeaabf8295a7681a14fd3467b0766d3ddbf782b
Yago Alexandre <goltarayago@gmail.com>
2023-03-15 19:49:27 -0300

    Problem solved -> next problem: version register bug

--- control-node/provision.sh
+++ control-node/provision.sh
@@ -3,8 +3,13 @@ echo "Installing EPEL"
 sudo yum install -y epel-release >/dev/null 2>&1
 echo "Installing Ansible"
 sudo yum install -y ansible >/dev/null 2>&1
+
+echo "Inserting hosts to /etc/hosts"
 cat <<EOT >> /etc/hosts
 192.168.56.2 control-node
 192.168.56.3 app01
 192.168.56.4 db01
 EOT
+
+echo "Inserting ansible hosts to /etc/ansible/hosts"
+sed -i '1s/^/[apps]\napp01\n[dbs]\ndb01\n/' /etc/ansible/hosts
